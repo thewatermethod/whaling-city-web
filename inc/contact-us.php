0d5f1ad4831f0ca0ea91410373a7491933f1b10d
Matt Bevilacqua <mbevilac@providence.edu>
2017-06-20 14:31:11 -0400

    more work on contact us

--- inc/contact-us.php
+++ inc/contact-us.php
@@ -1,22 +1,48 @@
 <?php
 
+//
+// here is the php for outputting a contact us form
+// the work of processing the form is handled in the javascript file "../js/contact-us.js"
+//
+
 function whaling_city_web_contact_us_form(){ ?>
 
 <form class="contact cf" onsubmit="event.preventDefault();"">
+  
   <h2>Let's Talk</h2>
+  
   <div class="half left cf">
     <input type="text" id="input-name" placeholder="Name" name="contactName">
     <input type="email" id="input-email" placeholder="Email address" name="contactEmail">
     <select id="input-subject" placeholder="Subject" name="contactSubject">
-    	<option value="1">I have a question</option>
-    	<option value="2">I would like to schedule a meeting?</option>
-    	<option value="3">I would like to see your pricing sheet</option>
+      <option value="1">I have a question</option>
+      <option value="2">I would like to schedule a meeting?</option>
+      <option value="3">I would like to see your pricing sheet</option>
     </select>
   </div>
+
   <div class="half right cf">
-    <textarea name="message" type="text" id="input-message" placeholder="Message"></textarea>
+   <textarea name="message" type="text" id="input-message" placeholder="Message"></textarea>
   </div>  
+
   <button id="input-submit">Get in Touch</button>
 </form>
 
 <?php }
+
+
+// this is the php that takes the ajax form and sends the email
+
+function whaling_city_web_contact_us_email(){
+
+  die("Hello World");
+  
+}
+
+// here are some hooks that we need to hook everything up
+
+// this tells admin-ajax what to do with this action
+add_action( 'wp_ajax_contact_us_email', 'whaling_city_web_contact_us_email' );
+
+// and this makes it so logged out users can use it as well
+add_action( 'wp_ajax_nopriv_contact_us_email', 'whaling_city_web_contact_us_email' );
--- js/contact-us.js
+++ js/contact-us.js
@@ -1,6 +1,3 @@
-//ajax.url
-
-
 window.onload = function(){
 
 	var contactFormSubmitButton = document.getElementById('input-submit')
@@ -10,6 +7,28 @@ window.onload = function(){
 
 function contactFormSubmit(){
 
+	var inputName = document.getElementById( 'input-name' ).value; 	
+	var inputEmail = document.getElementById( 'input-email' ).value;
+	var inputSubject = document.getElementById( 'input-subject' ).value; 
+	var inputMessage = document.getElementById( 'input-message' ).value;
+
+	var sendData = 'inputName=' + encodeURIComponent(inputName);
+	sendData += '&inputEmail=' + encodeURIComponent(inputEmail);
+	sendData += '&inputSubject=' + encodeURIComponent(inputSubject);
+	sendData += '&inputMessage=' +encodeURIComponent(inputMessage);
+
+	var xhr = new XMLHttpRequest();
+
+	xhr.onreadystatechange = function() { //Call a function when the state changes.
+    if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {    		
+        	alert('The server responded: ' + xhr.response);
+    	}
+	}
+
+	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	xhr.open( 'POST', ajax.url, true );
 	
-	
+
+	xhr.send( sendData );
+
 }
